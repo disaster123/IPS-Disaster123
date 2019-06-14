3f5a3e1262ea8300a0362055098d9844c89308c6
Stefan Priebe <spriebe@spriebe-laptop.workstation.phoffice>
2019-06-14 19:28:06 +0200

    init

---
+++ IPS-LandroidBridgeSend/module.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+class IPS_LandroidBridgeSend extends IPSModule
+{
+
+    public function Create()
+    {
+        //Never delete this line!
+        parent::Create();
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+    }
+
+    public function ApplyChanges()
+    {
+        //Never delete this line!
+        parent::ApplyChanges();
+        $this->ConnectParent('{C6D2AEB3-6E1F-4B2E-8E69-3A1A00246850}');
+    }
+
+    public function Send($jsonstr)
+    {
+	$this->SendDataToParent($jsonstr);
+    }
+}
